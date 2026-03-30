40d9c08ac882a5c48418a69400530dcbd0fa049c
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-03-30 21:12:51 +0800

    feat(neoscroll): add neoscroll plugin
    feat(neoscroll): add neoscroll plugin
    
    feat(neoscroll): make scroll faster

--- plugin/one-liners.lua
+++ plugin/one-liners.lua
@@ -1,3 +1,9 @@
 vim.pack.add({
   'https://github.com/tpope/vim-fugitive',
+  'https://github.com/karb94/neoscroll.nvim',
+})
+
+require('neoscroll').setup({
+  -- easing = 'sine',
+  duration_multiplier = 0.30,
 })
