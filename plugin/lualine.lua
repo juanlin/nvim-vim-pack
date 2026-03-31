3b22ac8cb2e71b61a9c98b28b04e7a9c9a708231
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-03-31 15:13:07 +0800

    refactor: wrap lualine in mini.misc safely()

--- plugin/lualine.lua
+++ plugin/lualine.lua
@@ -10,39 +10,44 @@ local function width_gt(limit)
   return vim.api.nvim_win_get_width(0) > limit
 end
 
-vim.pack.add({ 'https://github.com/nvim-lualine/lualine.nvim' })
+local misc = require('mini.misc')
+local later = function(f) misc.safely('later', f) end
 
-require('lualine').setup({
-  options = {
-    section_separators = '',
-    component_separators = '',
-    refresh = {
-      statusline = 75,  -- default 100ms
-    }
-  },
-  sections = {
-    lualine_a = {
-      {
-        'mode',
-        fmt = function(str)
-          if width_gt(70) then
-            return str
-          end
-          return initials(str)
-        end,
+later(function()
+  vim.pack.add({ 'https://github.com/nvim-lualine/lualine.nvim' })
+
+  require('lualine').setup({
+    options = {
+      section_separators = '',
+      component_separators = '',
+      refresh = {
+        statusline = 75,  -- default 100ms
+      }
+    },
+    sections = {
+      lualine_a = {
+        {
+          'mode',
+          fmt = function(str)
+            if width_gt(70) then
+              return str
+            end
+            return initials(str)
+          end,
+        },
+      },
+      lualine_b = {
+        { 'branch', icon = '' },
+        { 'diff' },
+        { 'diagnostics' },
+      },
+      lualine_x = {
+        { 'encoding', cond = function() return width_gt(75) end },
+        { 'fileformat', cond = function() return width_gt(70) end },
+        { 'filetype', cond = function() return width_gt(60) end },
       },
     },
-    lualine_b = {
-      { 'branch', icon = '' },
-      { 'diff' },
-      { 'diagnostics' },
-    },
-    lualine_x = {
-      { 'encoding', cond = function() return width_gt(75) end },
-      { 'fileformat', cond = function() return width_gt(70) end },
-      { 'filetype', cond = function() return width_gt(60) end },
-    },
-  },
-})
+  })
 
-vim.opt.showmode = false  -- hide mode notification
+  vim.opt.showmode = false  -- hide mode notification
+end)
