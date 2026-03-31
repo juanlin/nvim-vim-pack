7c7a577350025456975183572d9dbe9b075b50b6
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-03-31 15:55:25 +0800

    refactor: wrap lspconfig in mini.misc safely()

--- plugin/20-lspconfig.lua
+++ plugin/20-lspconfig.lua
@@ -1,62 +1,67 @@
-vim.pack.add({ 'https://github.com/neovim/nvim-lspconfig' })
+local misc = require('mini.misc')
+local later = function(f) misc.safely('later', f) end
 
--- Extra lua_ls settings not provided by nvim-lspconfig
-vim.lsp.config['lua_ls'] = {
-  settings = {
-    Lua = {
-      -- Specify NeoVim Lua
-      runtime = { version = 'LuaJIT', },
-      diagnostics = {
-        -- Specify vim and plugin globals
-        globals = { 'vim', 'MiniIcons', 'MiniFiles' }
+later(function()
+  vim.pack.add({ 'https://github.com/neovim/nvim-lspconfig' })
+
+  -- Extra lua_ls settings not provided by nvim-lspconfig
+  vim.lsp.config['lua_ls'] = {
+    settings = {
+      Lua = {
+        -- Specify NeoVim Lua
+        runtime = { version = 'LuaJIT', },
+        diagnostics = {
+          -- Specify vim and plugin globals
+          globals = { 'vim', 'MiniIcons', 'MiniFiles' }
+        },
+      },
+    }
+  }
+
+  -- Extra settings for rust_analyzer
+  vim.lsp.config['rust_analyzer'] = {
+    settings = {
+      ['rust-analyzer'] = {
+        -- Use clippy to check on save
+        check = { command = 'clippy' },
       },
     },
   }
-}
 
--- Extra settings for rust_analyzer
-vim.lsp.config['rust_analyzer'] = {
-  settings = {
-    ['rust-analyzer'] = {
-      -- Use clippy to check on save
-      check = { command = 'clippy' },
+  vim.lsp.enable({
+    'lua_ls',
+    'rust_analyzer',
+  })
+
+  -- Nicer borders on windows and popups
+  vim.o.winborder = 'rounded'
+  vim.o.pumborder = 'rounded'
+
+  vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('my-lsp-attach', { clear = true }),
+    callback = function(event)
+      -- Easier LSP mappings
+      local map = function(keys, func, desc, mode)
+        mode = mode or 'n'
+        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+      end
+
+      -- Rename the variable under cursor
+      map('grn', vim.lsp.buf.rename, 'Rename')
+
+      -- Execute a code action
+      map('gra', vim.lsp.buf.code_action, 'Goto Code Action', { 'n', 'x' })
+
+      -- Goto Declaration (not goto definition!)
+      map('grD', vim.lsp.buf.declaration, 'Goto Declaration')
+    end,
+  })
+
+  -- Diagnostics
+  vim.diagnostic.config({
+    virtual_lines = {
+      -- Only show virtual line diagnostics for the current cursor line
+      current_line = true,
     },
-  },
-}
-
-vim.lsp.enable({
-  'lua_ls',
-  'rust_analyzer',
-})
-
--- Nicer borders on windows and popups
-vim.o.winborder = 'rounded'
-vim.o.pumborder = 'rounded'
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('my-lsp-attach', { clear = true }),
-  callback = function(event)
-    -- Easier LSP mappings
-    local map = function(keys, func, desc, mode)
-      mode = mode or 'n'
-      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-    end
-
-    -- Rename the variable under cursor
-    map('grn', vim.lsp.buf.rename, 'Rename')
-
-    -- Execute a code action
-    map('gra', vim.lsp.buf.code_action, 'Goto Code Action', { 'n', 'x' })
-
-    -- Goto Declaration (not goto definition!)
-    map('grD', vim.lsp.buf.declaration, 'Goto Declaration')
-  end,
-})
-
--- Diagnostics
-vim.diagnostic.config({
-  virtual_lines = {
-   -- Only show virtual line diagnostics for the current cursor line
-   current_line = true,
-  },
-})
+  })
+end)
