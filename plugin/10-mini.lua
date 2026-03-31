a72f06cdf428226adcf39d4707cc73f8e8e44742
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-03-31 14:56:08 +0800

    refactor: move plugins to mini.misc safely()
    This cuts startuptime from ~50ms to ~35ms

--- plugin/10-mini.lua
+++ plugin/10-mini.lua
@@ -3,47 +3,62 @@ vim.pack.add({ 'https://github.com/nvim-mini/mini.nvim' })
 require('mini.icons').setup()
 MiniIcons.mock_nvim_web_devicons()
 
-require('mini.surround').setup({
-  -- Add g- prefix to avoid clobbering
-  mappings = {
-    add = 'gsa',
-    delete = 'gsd',
-    find = 'gsf',
-    find_left = 'gsF',
-    highlight = 'gsh',
-    replace = 'gsr',
-    update_n_lines = 'gsn',
-  },
-})
+require('mini.notify').setup()
 
 local misc = require('mini.misc')
 local later = function(f) misc.safely('later', f) end
+local on_event = function(ev, f) misc.safely('event:' .. ev, f) end
+
 later(function()
   require('mini.cmdline').setup()
+  require('mini.indentscope').setup({
+    symbol = '',
+    options = {
+      try_as_border = true,
+    }
+  })
+  require('mini.surround').setup({
+    mappings = {
+      -- Add g- prefix to avoid clobbering
+      add = 'gsa',
+      delete = 'gsd',
+      find = 'gsf',
+      find_left = 'gsF',
+      highlight = 'gsh',
+      replace = 'gsr',
+      update_n_lines = 'gsn',
+    },
+  })
+  require('mini.extra').setup() -- extra resources for picker (i.e.: oldfiles)
+  require('mini.pick').setup({
+    mappings = {
+      move_down = '<C-j>',
+      move_up = '<C-k>',
+    }
+  })
+  vim.keymap.set('n', '<leader>ff', '<Cmd>Pick files<CR>', { desc = 'Files' })
+  vim.keymap.set('n', '<leader>fg', '<Cmd>Pick grep_live<CR>', { desc = 'Grep live' })
+  vim.keymap.set('n', '<leader>fb', '<Cmd>Pick buffers<CR>', { desc = 'Buffers' })
+  vim.keymap.set('n', '<leader>fo', '<Cmd>Pick oldfiles<CR>', { desc = 'Old files' })
+  vim.keymap.set('n', '<leader>fh', '<Cmd>Pick history<CR>', { desc = 'History' })
+
+  vim.pack.add({
+    'https://github.com/tpope/vim-fugitive',
+    'https://github.com/karb94/neoscroll.nvim',
+    'https://github.com/brenoprata10/nvim-highlight-colors',
+  })
+  require('neoscroll').setup({
+    -- easing = 'sine',
+    duration_multiplier = 0.30,
+  })
+  require('nvim-highlight-colors').setup({
+    render = 'background',  -- background, foreground, virtual
+    virtual_symbol_position = 'eol' -- inline, eol, eow
+  })
+  require('nvim-highlight-colors').turnOff()
+  vim.keymap.set('n', '<leader>c', '<Cmd>HighlightColors Toggle<CR>', { desc = 'Toggle CSS colors' })
 end)
 
-require('mini.indentscope').setup({
-  symbol = '',
-  options = {
-    try_as_border = true,
-  }
-})
-
-require('mini.notify').setup()
-
--- Provide extra sources for picker (i.e.: oldfiles)
-require('mini.extra').setup()
-
-require('mini.pick').setup({
-  mappings = {
-    move_down = '<C-j>',
-    move_up = '<C-k>',
-  }
-})
-vim.keymap.set('n', '<leader>ff', '<Cmd>Pick files<CR>', { desc = 'Files' })
-vim.keymap.set('n', '<leader>fg', '<Cmd>Pick grep_live<CR>', { desc = 'Grep live' })
-vim.keymap.set('n', '<leader>fb', '<Cmd>Pick buffers<CR>', { desc = 'Buffers' })
-vim.keymap.set('n', '<leader>fo', '<Cmd>Pick oldfiles<CR>', { desc = 'Old files' })
-vim.keymap.set('n', '<leader>fh', '<Cmd>Pick history<CR>', { desc = 'History' })
-
-require('mini.completion').setup()
+on_event('InsertEnter', function()
+  require('mini.completion').setup()
+end)
--- plugin/one-liners.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add({
-  'https://github.com/tpope/vim-fugitive',
-  'https://github.com/karb94/neoscroll.nvim',
-})
-
-require('neoscroll').setup({
-  -- easing = 'sine',
-  duration_multiplier = 0.30,
-})
