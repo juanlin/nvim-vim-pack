ad6c7b2bc7fb80b28270d03291109f586da0be67
Juan Lin <9691057+juanlin@users.noreply.github.com>
2026-04-04 22:50:46 +0800

    feat(minipick): filter recent and history

--- plugin/10-mini.lua
+++ plugin/10-mini.lua
@@ -38,19 +38,88 @@ later(function()
     },
   })
 
-  -- Mini Pick and Mini Extras 
-  require('mini.extra').setup() -- extra resources for picker (i.e.: oldfiles)
-  require('mini.pick').setup({
+  -- Mini Pick
+  local MiniPick = require('mini.pick')
+
+  MiniPick.setup({
     mappings = {
       move_down = '<C-j>',
       move_up = '<C-k>',
     }
   })
+
+  -- Pick Recent Files (clean: no current files, no git msg, no URIs)
+  local pick_recentfiles_clean = function()
+    -- Get a list of normalized paths currently visible in windows
+    local visible_files = {}
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      local buf = vim.api.nvim_win_get_buf(win)
+      local name = vim.api.nvim_buf_get_name(buf)
+      if name ~= '' then
+        visible_files[vim.fs.normalize(name)] = true
+      end
+    end
+
+    local all_recentfiles = vim.v.oldfiles
+
+    local filtered_recentfiles = vim.tbl_filter(function(path)
+      -- Check if normalized path is in our "visible" lookup table
+      if visible_files[vim.fs.normalize(path)] then return false end
+
+      -- Check if it's a commit message
+      if path:find('.git/COMMIT_EDITMSG', 1, true) then return false end
+
+      -- Check for special plugin/internal URIs
+      if path:find('nvim-pack://', 1, true) or
+        path:find('fugitive://', 1, true) or
+        path:find('health://', 1, true) then
+        return false
+      end
+
+      return true
+    end, all_recentfiles)
+
+    MiniPick.start({
+      source = {
+        items = filtered_recentfiles,
+        name = 'Recent Files (Clean)',
+        choose = MiniPick.default_choose,
+      },
+    })
+  end
+
+  -- Pick Command History
+  local pick_command_history = function()
+    local history_list = {}
+    local seen = {}   -- table to track duplicates
+    local history_count = vim.fn.histnr(':')
+
+    -- Build the list from newest to oldest
+    for i = history_count, 1, -1 do
+      local cmd = vim.fn.histget(':', i)
+      -- No empty, duplicates, or short commands
+      if cmd ~= '' and not seen[cmd] and #cmd >2 then
+        table.insert(history_list, cmd)
+        seen[cmd] = true  -- mark as seen
+      end
+    end
+
+    MiniPick.start({
+      source = {
+        items = history_list,
+        name = 'Command History',
+        choose = function(item)
+          vim.api.nvim_feedkeys(':' .. item, 'n', true)
+        end,
+      },
+    })
+  end
+
   vim.keymap.set('n', '<leader>ff', '<Cmd>Pick files<CR>', { desc = 'Files' })
   vim.keymap.set('n', '<leader>fg', '<Cmd>Pick grep_live<CR>', { desc = 'Grep live' })
   vim.keymap.set('n', '<leader>fb', '<Cmd>Pick buffers<CR>', { desc = 'Buffers' })
-  vim.keymap.set('n', '<leader>fo', '<Cmd>Pick oldfiles<CR>', { desc = 'Old files' })
-  vim.keymap.set('n', '<leader>fh', '<Cmd>Pick history<CR>', { desc = 'History' })
+  vim.keymap.set('n', '<leader>fo', pick_recentfiles_clean, { desc = 'Recent Files' })
+  vim.keymap.set('n', '<leader>fh', pick_command_history, { desc = 'Command History' })
 
   -- OTHER non-mini plugins
   vim.pack.add({
